3d324d21606cfd25c889187f4964c7df4a1c8906
Lucas Fonseca <lucasrfon@gmail.com>
2022-10-21 17:04:43 -0300

    feat: add text from payment

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,15 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+      <p>Primeiro, escolha sua modalidade de ingresso</p>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
